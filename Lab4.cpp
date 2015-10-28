d4a0839004a1ce344addb7ad8200f0e9c9582750
Yaskovich Dmitry <kadavr95@outlook.com>
2015-10-28 11:05:00 +0300

    code style changes

--- Lab4.cpp
+++ Lab4.cpp
@@ -3,24 +3,26 @@
 #include <conio.h>
 #include <math.h>
 
-void main( )
+void main()
 {
-  double x, a, s, n, j;
-  int i,p;
-  cout<<"Enter x, accuracy: ";
-  cin>>x>>a;
-  s=i=n=1;
-  while (a<=fabs(n))
-  {
-		n=n*(-1)*(4*i-5)/(4*i)*x;
-		s=s+n;
-		i++;
-  }
-  p=0;
-  for (j=a; j<1; j=j*10)
-		p++;
-  cout<<setiosflags(ios::fixed)<<setprecision(p)<<"Series result: "<<s;
-  s=pow(1+x, 0.25);
-  cout<<setiosflags(ios::fixed)<<setprecision(p)<<" Function result: "<<s;
-  getch();
+	double x,a,s,n,j;
+	int i,p;
+	cout<<"Enter x, accuracy: ";
+	cin>>x>>a;
+	s=i=n=1;
+	while (a<=fabs(n))
+		{
+			n=n*(-1)*(4*i-5)/(4*i)*x;
+			s=s+n;
+			i++;
+		}
+	p=0;
+	for (j=a; j<1; j=j*10)
+		{
+			p++;
+		}
+	cout<<setiosflags(ios::fixed)<<setprecision(p)<<"Series result: "<<s;
+	s=pow(1+x, 0.25);
+	cout<<setiosflags(ios::fixed)<<setprecision(p)<<" Function result: "<<s;
+	getch();
 }
